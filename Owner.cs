bdfd48bfb3db770af457f42083811287ec0cfabe
jega3005 <160155359+jega3005@users.noreply.github.com>
2024-10-11 13:16:04 +0530

    Add files via upload

---
+++ Owner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class Owner
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<Book> Books { get; set; }
+}
+
+public class Book
+{
+    public string Name { get; set; }
+    public string Type { get; set; } 
+}
